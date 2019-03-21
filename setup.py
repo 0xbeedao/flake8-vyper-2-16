b91140abf4ba7e7823565bb075f52e0c944755ab
Mike Shultz <shultzm@gmail.com>
2019-03-21 16:52:54 -0600

    v0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="flake8_vyper",
     license="MIT",
-    version="0.1.0",
+    version="0.1.1",
     description="Plugin for flake8 to support Vyper",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- vyper_filter.py
+++ vyper_filter.py
@@ -2,7 +2,7 @@ from flake8.formatting.default import Default
 
 __author__ = 'Mike Shultz'
 __email__ = 'mike@mikeshultz.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 class VyperFilterPlugin(Default):
